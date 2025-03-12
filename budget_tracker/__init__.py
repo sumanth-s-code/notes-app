aa05027c63ad213835507e099433783dfeafe0f0
Markondapatnaikuni Sai Venkata Sumanth <saivenkata.sumanth@saivenkata.sumanth>
2025-03-12 08:16:03 +0530

    Removed unnecessary files

--- budget_tracker/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Budget Tracker Package
-This package provides modules for handling database operations(db.py) and the command line interface(cli.py)
-for the Budget tracker app
-"""
-from .import db
-from .import cli
